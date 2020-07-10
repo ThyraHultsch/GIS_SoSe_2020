f0a86dbbb4b81c4fbc7a5501ea20e45791bd4772
ThyraHultsch <64130749+ThyraHultsch@users.noreply.github.com>
2020-07-10 16:59:49 +0200

    push

--- Aufgabe11/server.js
+++ Aufgabe11/server.js
@@ -3,17 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Aufgabe11 = void 0;
 const Http = require("http");
 const Url = require("url");
-const Mongo = require("mongodb");
+const Mongo = require("mongodb"); //Imports
 var Aufgabe11;
 (function (Aufgabe11) {
-    let formularData;
+    let formularData; //Datenbank Mongo Collection 
     let databaseUrl;
-    let myArgs = process.argv.slice(2);
+    let myArgs = process.argv.slice(2); //abfragen obs lokal oder remote ist und alles was hintendran steht kommt in den Array
     if (myArgs[0] == "local") {
         databaseUrl = "mongodb://localhost:27017";
     }
     else {
-        databaseUrl = "mongodb+srv://Testuser:<password>@cluster0.whstr.mongodb.net/<dbname>?retryWrites=true&w=majority";
+        databaseUrl = "mongodb+srv://Testuser:testuser123@cluster0.whstr.mongodb.net/Aufgabe11?retryWrites=true&w=majority";
     }
     connectToDatabase(databaseUrl);
     let port = Number(process.env.PORT);
@@ -26,25 +26,25 @@ var Aufgabe11;
     async function connectToDatabase(_url) {
         let options = { useNewUrlParser: true, useUnifiedTopology: true };
         let mongoClient = new Mongo.MongoClient(_url, options);
-        await mongoClient.connect();
-        formularData = mongoClient.db("Aufgabe11").collection("formular_eingabe");
+        await mongoClient.connect(); //Mongoclient connecten
+        formularData = mongoClient.db("Aufgabe11").collection("Formular"); //Name Datenbank und Collection Name damit man drauf zugreifen kann
     }
     function handleRequest(_request, _response) {
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.setHeader("content-type", "text/html; charset=utf-8");
-        if (_request.url) {
+        if (_request.url) { //mitgeschicktes Parsen
             let url = Url.parse(_request.url, true);
             let path = url.pathname;
             if (path == "/retrieve") {
                 formularData.find({}).toArray(function (err, result) {
                     if (err)
                         throw err;
-                    let resultString = "";
+                    let resultString = ""; //leerer String damit man was reinmachen kann 
                     for (let i = 0; i < result.length; i++) {
                         resultString += JSON.stringify(result[i]) + ",";
                     }
                     console.log(resultString);
-                    _response.write(JSON.stringify(resultString));
+                    _response.write(JSON.stringify(resultString)); //wird als json string ausgegeben
                     _response.end();
                 });
             }
--- Aufgabe11/server.ts
+++ Aufgabe11/server.ts
@@ -1,17 +1,17 @@
 import * as Http from "http";
 import * as Url from "url";
-import * as Mongo from "mongodb";
+import * as Mongo from "mongodb"; //Imports
 
 export namespace Aufgabe11 {
 
-    let formularData: Mongo.Collection;
+    let formularData: Mongo.Collection; //Datenbank Mongo Collection 
     let databaseUrl: string;
 
-    let myArgs: string[] = process.argv.slice(2);
+    let myArgs: string[] = process.argv.slice(2); //abfragen obs lokal oder remote ist und alles was hintendran steht kommt in den Array
     if (myArgs[0] == "local") {
         databaseUrl = "mongodb://localhost:27017";
     } else {
-        databaseUrl = "mongodb+srv://Testuser:<password>@cluster0.whstr.mongodb.net/<dbname>?retryWrites=true&w=majority";
+        databaseUrl = "mongodb+srv://Testuser:testuser123@cluster0.whstr.mongodb.net/Aufgabe11?retryWrites=true&w=majority";
     }
 
 
@@ -28,31 +28,31 @@ export namespace Aufgabe11 {
 
     async function connectToDatabase(_url: string): Promise<void> {
         let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
-        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
-        await mongoClient.connect();
-        formularData = mongoClient.db("Aufgabe11").collection("formular_eingabe");
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);    
+        await mongoClient.connect();      //Mongoclient connecten
+        formularData = mongoClient.db("Aufgabe11").collection("Formular");  //Name Datenbank und Collection Name damit man drauf zugreifen kann
     }
     
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
 
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("content-type", "text/html; charset=utf-8");  
     
-        if (_request.url) {
+        if (_request.url) {   //mitgeschicktes Parsen
           let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
           let path: string | null = url.pathname;
           if (path == "/retrieve") {
-            formularData.find({}).toArray(function(err: Mongo.MongoError, result: string[]): void {
+            formularData.find({}).toArray(function(err: Mongo.MongoError, result: string[]): void {   //alles was du findest zum Array machen
             if (err)
               throw err;
             
-            let resultString: string = "";
+            let resultString: string = "";    //leerer String damit man was reinmachen kann 
             for (let i: number = 0; i < result.length; i++) {
-              resultString += JSON.stringify(result[i]) + ",";
+              resultString += JSON.stringify(result[i]) + ",";  
             }
     
             console.log(resultString);
-            _response.write(JSON.stringify(resultString));
+            _response.write(JSON.stringify(resultString));    //wird als json string ausgegeben
             _response.end();
             });
             }
